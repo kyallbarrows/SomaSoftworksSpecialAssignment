14106dfaede5ab726591bb48b2aca1429bf0a0d0
Kyall Barrows <kyallb@gmail.com>
2025-10-18 21:07:18 -0500

    Safety checkin

---
+++ Assets/Scripts/InvestigativeTooling/ToolingNavigator.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+namespace SpecialAssignment
+{
+    public class ToolingNavigator : MonoBehaviour
+    {
+        public enum ToolingType
+        {
+            VoicePrintAnalysis = 0,
+            LocationTracking = 1,
+            FacialRecognition = 2,
+        }
+
+        [SerializeField] private ToolingType _toolingType;
+        [SerializeField] private 
+    }
+}
